4e93cb28e0751cf6714e561287a7615900f2940e
khatija haneef <khatija.haneef@gmail.com>
2021-11-05 06:07:48 +0000

    env config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+require('dotenv').config({ path: './.env' });
 //const {google} = require("googleapis");
 //var smtpTransport = require('nodemailer-smtp-transport');
 
